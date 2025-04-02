4d743fadddb82152a6acd97c60088ca92539292d
JarlJEB <julian@skillhouse.no>
2025-04-02 11:15:59 +0200

    🎨 Gjenopprettet designbase før AI-integrasjon

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,16 @@
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
-  ],
+  content: ["./pages/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif']
+      },
       colors: {
-        skillgreen: "#2bb77b",
-        skillwhite: "#fcfcf2"
+        skillgray: '#2e2f32',
+        skillgreen: '#2bb77b',
+        skillwhite: '#fcfcf2'
       }
-    }
+    },
   },
-  plugins: []
+  plugins: [],
 }
